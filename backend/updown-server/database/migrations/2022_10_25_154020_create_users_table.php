579924eb3f098173b4ddbb195984c683a851c52d
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-27 12:54:53 +0300

    creating users tabel schema

--- backend/updown-server/database/migrations/2022_10_25_154020_create_users_table.php
+++ backend/updown-server/database/migrations/2022_10_25_154020_create_users_table.php
@@ -15,12 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('phone_number')->nullable();
+            $table->integer('age')->nullable();
+            $table->boolean('is_banned')->default(false);
+            $table->foreign('user_type_id')->references('id')->on('user_types')->onDelete('set null');
+            $table->timestamp('created_at')->useCurrent();
         });
     }
 
