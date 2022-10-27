85bd51bef7ea8e06219998831ab8d37b06c76e8f
Hadi Darwish <hadi.darwish.03@gmail.com>
2022-10-27 13:06:15 +0300

    Backend => creating buildings tabel schema

---
+++ backend/updown-server/database/migrations/2022_10_27_100228_create_buildings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->integer('number_of_floors');
+            $table->integer('number_of_apartments');
+            $table->boolean('is_paid')->default(false);
+            $table->foreign('owner_id')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings');
+    }
+};
