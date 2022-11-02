7168fe592d89d013536ef01c24a648c51f91656e
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-02 14:09:41 +0200

    Backend => adding getUser function into user controller

--- backend/updown-server/app/Http/Controllers/UserController.php
+++ backend/updown-server/app/Http/Controllers/UserController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use  Illuminate\Database\Eloquent;
 
 class UserController extends Controller
 {
-    //
+    public function getUser($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'User not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+        ]);
+    }
 }
