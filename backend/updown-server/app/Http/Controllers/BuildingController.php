a42c2027c75986e5953ebe8d14cd17aeb16cbdb3
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-03 07:34:50 +0200

    Backend => adding getBuilding function into building controller

--- backend/updown-server/app/Http/Controllers/BuildingController.php
+++ backend/updown-server/app/Http/Controllers/BuildingController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Building;
 use Illuminate\Http\Request;
+use  Illuminate\Database\Eloquent;
 
 class BuildingController extends Controller
 {
@@ -10,6 +12,34 @@ class BuildingController extends Controller
     public function createBuilding(Request $request)
     {
         $building = Building::create($request->all());
-        return response()->json($building, 201);
+        if (
+            $building->save()
+        ) {
+            return response()->json([
+                'status' => 'success',
+                'building' => $building,
+            ], 201);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Building not created',
+            ], 404);
+        }
+    }
+
+    public function getBuilding($id)
+    {
+
+        $building = Building::find($id);
+        if (!$building) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Building not found',
+            ], 404);
+        }
+        return response()->json([
+            'status' => 'success',
+            'building' => $building,
+        ]);
     }
 }
