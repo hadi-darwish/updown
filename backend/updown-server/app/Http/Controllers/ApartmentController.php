1c2c92407ce89b4df62abb6083106be306248b37
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-03 07:55:31 +0200

    Backend => adding triggerBanApartment function into apartment controller

--- backend/updown-server/app/Http/Controllers/ApartmentController.php
+++ backend/updown-server/app/Http/Controllers/ApartmentController.php
@@ -99,4 +99,27 @@ class ApartmentController extends Controller
             ], 404);
         }
     }
+
+    public function triggerBanApartment($id)
+    {
+        $apartment = Apartment::find($id);
+        if (!$apartment) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Apartment not found',
+            ], 404);
+        }
+        $apartment->is_banned = !$apartment->is_banned;
+        if ($apartment->save()) {
+            return response()->json([
+                'status' => 'success',
+                'apartment' => $apartment,
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Apartment not updated',
+            ], 404);
+        }
+    }
 }
