982f76e54400388a7851b88168d4acc235ddae2e
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-03 08:08:40 +0200

    Backend => adding createPrice function into price controller

--- backend/updown-server/app/Http/Controllers/PriceController.php
+++ backend/updown-server/app/Http/Controllers/PriceController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PriceController extends Controller
 {
-    //
+    public function createPrice(Request $request)
+    {
+        $price = Price::create($request->all());
+        if ($price->save()) {
+            return response()->json([
+                'status' => 'success',
+                'price' => $price,
+            ], 201);
+        } else {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Price not created',
+            ], 404);
+        }
+    }
 }
