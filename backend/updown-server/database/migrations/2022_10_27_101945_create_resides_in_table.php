2a4342820d8350cad03d814b3ebde4361bccce8e
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-16 21:54:49 +0200

    Backend => fixing resides_in migration schema

--- backend/updown-server/database/migrations/2022_10_27_101945_create_resides_in_table.php
+++ backend/updown-server/database/migrations/2022_10_27_101945_create_resides_in_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('resides_in', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete(' cascade');
             $table->foreignId('apartment_id')->constrained('apartments')->onDelete('cascade');
         });
