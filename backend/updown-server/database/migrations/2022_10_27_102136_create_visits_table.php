c4a8b49094088389fa5e704f7ea03b409059615b
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-16 21:52:03 +0200

    Backend => fixing visits migration schema

--- backend/updown-server/database/migrations/2022_10_27_102136_create_visits_table.php
+++ backend/updown-server/database/migrations/2022_10_27_102136_create_visits_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
     {
         Schema::create('visits', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('visitor_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('set null');
+            $table->string('visitor_email')->nullable();
+            $table->string('code')->nullable();
             $table->timestamp('date')->useCurrent();
         });
     }
