a059ff018e899a78cec42b4ecf1ea5f6cb1f628f
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-16 22:01:40 +0200

    Backend => adding visit controller

--- backend/updown-server/app/Models/Visit.php
+++ backend/updown-server/app/Models/Visit.php
@@ -4,8 +4,40 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Visit extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [
+        'user_id',
+        'visitor_email',
+        'code',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     */
+    protected $hidden = [
+        'user_id',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     */
+    protected $casts = [
+        'date' => 'datetime',
+    ];
+
+    /**
+     * Get the user that owns the Visit
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
