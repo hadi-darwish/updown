730ad771f285556576bcd3db5ed215af33e004b4
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-16 19:11:48 +0200

    Frontend - Mobile => adding email validator

--- frontend/mobile/updown/validators/email_validator.dart
+++ frontend/mobile/updown/validators/email_validator.dart
@@ -1,1 +1,11 @@
-
+class EmailValidator {
+  static String validate(String value) {
+    if (value.isEmpty) {
+      return 'Email is required';
+    }
+    if (!RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+').hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return '';
+  }
+}
