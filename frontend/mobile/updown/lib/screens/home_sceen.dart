36d5d590db03612d4987d22d26f45e24bd09e7ee
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-17 09:36:33 +0200

    Frontend - Mobile => adding nav bar widget

--- frontend/mobile/updown/lib/screens/home_sceen.dart
+++ frontend/mobile/updown/lib/screens/home_sceen.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:updown/widgets/nav_bar.dart';
+import 'package:updown/widgets/top_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,8 +10,35 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int selectedPage = 0;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: TopBar(screenHeight: screenHeight),
+      body: IndexedStack(
+        index: selectedPage,
+        children: [
+          Container(
+            child: const Text('Home'),
+          ),
+          Container(
+            child: const Text('Stats'),
+          ),
+          Container(
+            child: const Text('Settings'),
+          ),
+        ],
+      ),
+      bottomNavigationBar:
+          NavBar(currentIndex: selectedPage, onTaped: onTabTapped),
+    );
+  }
+
+  void onTabTapped(int currentIndex) {
+    setState(() {
+      selectedPage = currentIndex;
+    });
   }
 }
