dd04d8f77399e6a16e93de648efafa9dd109116e
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-17 16:29:25 +0200

    Frontend - Mobile => adding elevator button widget

--- frontend/mobile/updown/lib/widgets/elevator_button.dart
+++ frontend/mobile/updown/lib/widgets/elevator_button.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class ElevatorButton extends StatelessWidget {
+  const ElevatorButton({super.key, required this.floorNumber});
+  final int floorNumber;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return ElevatedButton(
+      onPressed: () {},
+      style: ButtonStyle(
+        backgroundColor:
+            MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            side: BorderSide(color: Theme.of(context).primaryColor),
+          ),
+        ),
+        minimumSize: MaterialStateProperty.all<Size>(Size(70, 50)),
+        maximumSize: MaterialStateProperty.all<Size>(Size(70, 50)),
+      ),
+      child: Text(
+        floorNumber == 0 ? 'GF' : '$floorNumber',
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 32,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+    );
   }
 }
