f02a4d7f206f1bca93060282effb7b92b1b87cbb
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-17 13:13:33 +0200

    Frontend - Mobile => styling text box widget

--- frontend/mobile/updown/lib/widgets/text_box.dart
+++ frontend/mobile/updown/lib/widgets/text_box.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 
 class TextBox extends StatelessWidget {
-  const TextBox({super.key});
+  const TextBox(
+      {super.key,
+      required this.text1,
+      required this.text2,
+      required this.theme});
+  final String text1;
+  final String text2;
+  final String theme;
 
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
-          color: Theme.of(context).secondaryHeaderColor,
+          color: theme == 'primary'
+              ? Theme.of(context).primaryColor
+              : Theme.of(context).secondaryHeaderColor,
           borderRadius: const BorderRadius.all(Radius.circular(5)),
         ),
         width: 340,
@@ -21,16 +30,20 @@ class TextBox extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Text(
-              'Current Floor',
+              text1,
               style: TextStyle(
-                color: Theme.of(context).primaryColor,
+                color: theme == 'primary'
+                    ? Colors.white
+                    : Theme.of(context).primaryColor,
                 fontSize: 14,
               ),
             ),
             Text(
-              '  1',
+              text2,
               style: TextStyle(
-                color: Theme.of(context).primaryColor,
+                color: theme == 'primary'
+                    ? Colors.white
+                    : Theme.of(context).primaryColor,
                 fontSize: 14,
                 fontWeight: FontWeight.w900,
               ),
