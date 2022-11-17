2320977f5d115cd748330975c7f4d05adf5fc0d1
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-17 12:48:39 +0200

    Frontend - Mobile => styling small top widget

--- frontend/mobile/updown/lib/widgets/small_top.dart
+++ frontend/mobile/updown/lib/widgets/small_top.dart
@@ -5,17 +5,55 @@ class ElevatorStatus extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 24,
+        vertical: 15,
+      ),
+      decoration: const BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 10,
+            offset: Offset(0, 5),
+          ),
+        ],
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(
+            color: Color.fromRGBO(197, 197, 197, 1),
+            width: 1,
+          ),
+        ),
+      ),
       child: Row(
         mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: const [
-          Text('Building Name'),
-          Text('Elevator'),
-          Icon(
-            Icons.circle,
-            color: Colors.green,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Building Name',
+            style: TextStyle(
+              color: Theme.of(context).primaryColor,
+              fontSize: 14,
+            ),
           ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Elevator  ',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w900,
+                  )),
+              const Icon(
+                Icons.circle,
+                color: Colors.green,
+                size: 14,
+              ),
+            ],
+          )
         ], // this will take up the space of the row
       ),
     );
