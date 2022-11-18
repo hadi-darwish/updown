89618403a4fe0aa01290001ee92f0dbb4635d93b
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-18 21:38:58 +0200

    Frontend - Mobile => adding access screen

---
+++ frontend/mobile/updown/lib/screens/access.sceen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AccessPage extends StatefulWidget {
+  const AccessPage({super.key});
+
+  @override
+  State<AccessPage> createState() => _AccessPageState();
+}
+
+class _AccessPageState extends State<AccessPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
