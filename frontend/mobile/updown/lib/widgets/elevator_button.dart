ed39dc10ced8246d5d67d086fa84b390f9363311
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-18 17:39:56 +0200

    Frontend - Mobile => styling diabled elevator button

--- frontend/mobile/updown/lib/widgets/elevator_button.dart
+++ frontend/mobile/updown/lib/widgets/elevator_button.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:updown/providers/travel_provider.dart';
 
 class ElevatorButton extends StatefulWidget {
-  const ElevatorButton({super.key, required this.floorNumber});
+  const ElevatorButton(
+      {super.key,
+      required this.floorNumber,
+      required this.currentFloor,
+      required this.disabled});
   final int floorNumber;
+  final int currentFloor;
+  final bool disabled;
 
   @override
   State<ElevatorButton> createState() => _ElevatorButtonState();
@@ -16,22 +23,56 @@ class _ElevatorButtonState extends State<ElevatorButton> {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return ElevatedButton(
-      onPressed: () {
-        setState(() {
-          isPressed = !isPressed;
-        });
+      onPressed: () async {
+        if (!widget.disabled) {
+          setState(() {
+            isPressed = !isPressed;
+          });
+          await Travel().requestTravel(widget.floorNumber, widget.currentFloor);
+          Future delayed = Future.delayed(const Duration(seconds: 1), () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: const Text('Travel Request'),
+                  content: const Text('Your travel request has been sent'),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('OK'),
+                    ),
+                  ],
+                );
+              },
+            );
+            setState(() {
+              isPressed = !isPressed;
+            });
+          });
+        } else {
+          null;
+        }
       },
       style: ButtonStyle(
-        backgroundColor:
-            MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
+        overlayColor: !widget.disabled
+            ? null
+            : MaterialStateProperty.all<Color>(Colors.red.withOpacity(1)),
+        backgroundColor: widget.disabled
+            ? MaterialStateProperty.all<Color>(
+                Theme.of(context).primaryColor.withOpacity(0.5))
+            : MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(5.0),
             side: BorderSide(
                 width: 7.5,
-                color: isPressed
-                    ? Theme.of(context).secondaryHeaderColor
-                    : Theme.of(context).primaryColor),
+                color: widget.disabled
+                    ? Theme.of(context).primaryColor.withOpacity(0.2)
+                    : isPressed
+                        ? Theme.of(context).secondaryHeaderColor
+                        : Theme.of(context).primaryColor),
           ),
         ),
         minimumSize: MaterialStateProperty.all<Size>(Size(70, 50)),
