42937004a6f64dbf14b6264117ad1275a878ae2b
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-19 03:35:17 +0200

    Frontend - Mobile => adding refresh function

--- frontend/mobile/updown/lib/screens/stats_screen.dart
+++ frontend/mobile/updown/lib/screens/stats_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:updown/providers/auth_provider.dart';
+import 'package:updown/providers/building_provider.dart';
 import 'package:updown/widgets/small_top.dart';
 import 'package:updown/widgets/stat_box.dart';
 import 'dart:async';
@@ -24,6 +26,7 @@ class _StatsPageState extends State<StatsPage> {
   int number_of_guests = 0;
   int total = 0;
   String status = 'OFF';
+  int x = 1;
 
   @override
   void initState() {
@@ -69,36 +72,88 @@ class _StatsPageState extends State<StatsPage> {
     prices();
   }
 
+  Future refresh() async {
+    setState(() {
+      Provider.of<Building>(context, listen: false).getBuilding();
+      Future<void> prices() async {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        String? token = prefs.getString('token');
+        print(prefs.getString('token'));
+        int? building_id = prefs.get('building_id') as int?;
+        final url = Uri.parse('${dotenv.env['BASE_URL']}prices');
+        try {
+          final response = await http.post(
+            url,
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer $token',
+            },
+            body: json.encode(
+              {
+                'building_id': building_id,
+              },
+            ),
+          );
+          final responseData = json.decode(response.body);
+          setState(() {
+            print(responseData['prices'][0]['price_per_travel']);
+            tax = responseData['prices'][0]['tax'];
+            price_per_travel = responseData['prices'][0]['price_per_travel'];
+            total_travels = responseData['travels'];
+            number_of_guests = responseData['guests'];
+            total = (total_travels * price_per_travel) + tax;
+            status = prefs.getBool('buildingStatus')! ? 'ON' : 'OFF';
+          });
+          // print(tax);
+        } catch (error) {
+          print(
+              '8allllllllllllllllllllllllaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaat');
+          rethrow;
+        }
+      }
+
+      prices();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Theme.of(context).backgroundColor,
-      ),
-      child: Column(
-        children: [
-          ElevatorStatus(),
-          SizedBox(
-            height: 20,
-          ),
-          GridView.custom(
-            shrinkWrap: true,
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
+    Timer mytimer = Timer.periodic(const Duration(seconds: 30), (timer) {
+      //code to run on every 5 seconds
+      Provider.of<Building>(context, listen: false).getBuilding();
+      // refresh();
+    });
+    return RefreshIndicator(
+      onRefresh: refresh,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).backgroundColor,
+        ),
+        child: Column(
+          children: [
+            const ElevatorStatus(),
+            const SizedBox(
+              height: 20,
             ),
-            childrenDelegate: SliverChildListDelegate(
-              [
-                StatBox(title: 'Travel cost', content: '\$$price_per_travel'),
-                StatBox(title: 'Travel count', content: '$total_travels'),
-                StatBox(title: 'Tax', content: '\$$tax'),
-                StatBox(title: 'Guests', content: '$number_of_guests'),
-                StatBox(title: 'Total', content: '\$$total'),
-                StatBox(title: 'Travel cost', content: status),
-              ],
+            GridView.custom(
+              shrinkWrap: true,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+              ),
+              childrenDelegate: SliverChildListDelegate(
+                [
+                  StatBox(title: 'Travel cost', content: '\$$price_per_travel'),
+                  StatBox(title: 'Travel count', content: '$total_travels'),
+                  StatBox(title: 'Tax', content: '\$$tax'),
+                  StatBox(title: 'Guests', content: '$number_of_guests'),
+                  StatBox(title: 'Total', content: '\$$total'),
+                  StatBox(title: 'Travel cost', content: status),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
