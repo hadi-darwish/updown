f9fbe1a38fee24240f7d71f3cd50a80c8118b937
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-19 08:31:58 +0200

    Frontend - Web => adding login page

---
+++ frontend/web/updown/src/components/Button/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = ({ width, mode, onClick, text }) => {
+  return (
+    <button
+      className={`button ${mode}`}
+      style={{ width: width }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
