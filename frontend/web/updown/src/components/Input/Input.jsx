ab3184935a5217b4e7b61e996c2e5f4e249f9fd8
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-19 15:29:12 +0200

    Frontend - Web => fixing input styles

--- frontend/web/updown/src/components/Input/Input.jsx
+++ frontend/web/updown/src/components/Input/Input.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Input.css";
 
-const Input = ({ label, type, placeholder }) => {
+const Input = ({ label, type, placeholder, onChange }) => {
   return (
     <div className="input-container">
-      <p className="input-label">{label}</p>
-      <input className="input" type={type} placeholder={placeholder} />
+      <div className="center">
+        {" "}
+        <label className="input-label">{label}</label>
+        <input
+          className="input"
+          type={type}
+          placeholder={placeholder}
+          onChange={onChange}
+        />
+      </div>
     </div>
   );
 };
