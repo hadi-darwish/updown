a47a90f5ea86b63411c35901b5f28e363abbf758
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-19 03:54:32 +0200

    Frontend - Mobile => adding main file

--- frontend/mobile/updown/lib/main.dart
+++ frontend/mobile/updown/lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:updown/providers/building_provider.dart';
+import 'package:updown/providers/guest_provider.dart';
+import 'package:updown/screens/guest_mode_login_screen.dart';
+import 'package:updown/screens/guest_screen.dart';
+import 'package:updown/screens/home_sceen.dart';
+import 'package:updown/screens/sign_in_screen.dart';
+
 import 'package:updown/widgets/button.dart';
 import 'package:updown/widgets/input.dart';
 import 'package:updown/widgets/top_bar.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(
+      fileName: '.env'); // This is where you can store your API keys
   runApp(const MyApp());
 }
 
@@ -16,65 +27,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Nunito',
-        cardColor: const Color.fromRGBO(89, 219, 174, 1),
-        primaryColor: const Color.fromRGBO(0, 48, 63, 1),
-        primaryColorLight: const Color.fromRGBO(0, 48, 63, 0.5),
-        secondaryHeaderColor: const Color.fromRGBO(89, 219, 174, 1),
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Color.fromRGBO(89, 219, 174, 1),
-          selectionColor: Color.fromRGBO(89, 219, 174, 1),
-          selectionHandleColor: Color.fromRGBO(0, 48, 63, 1),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => Guest()),
+        ChangeNotifierProvider(create: (context) => Building()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          backgroundColor: const Color.fromRGBO(245, 244, 244, 1),
+          fontFamily: 'Nunito',
+          cardColor: const Color.fromRGBO(89, 219, 174, 1),
+          primaryColor: const Color.fromRGBO(0, 48, 63, 1),
+          primaryColorLight: const Color.fromRGBO(0, 48, 63, 0.5),
+          secondaryHeaderColor: const Color.fromRGBO(89, 219, 174, 1),
+          textSelectionTheme: const TextSelectionThemeData(
+            cursorColor: Color.fromRGBO(89, 219, 174, 1),
+            selectionColor: Color.fromRGBO(89, 219, 174, 1),
+            selectionHandleColor: Color.fromRGBO(0, 48, 63, 1),
+          ),
         ),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
-    final textController = TextEditingController();
-    final passwordController = TextEditingController();
-    return Scaffold(
-      appBar: TopBar(
-        screenHeight: screenHeight,
-      ),
-      body: Column(
-        children: [
-          LabeledInput(
-            placeholder: 'Email',
-            type: 'Email',
-            textController: textController,
-          ),
-          LabeledInput(
-              placeholder: 'Password',
-              type: 'Password',
-              textController: passwordController),
-          Button(
-            text: 'Login',
-            onPressed: () {
-              print(textController.text);
-              print(passwordController.text);
-            },
-            type: 'secondary',
-          ),
-        ],
+        home: const SignIn(),
+        routes: {
+          '/signIn': (context) => const SignIn(),
+          '/guestModeLogin': (context) => const GuestModeLogin(),
+          '/home': (context) => const Home(),
+          '/guestMode': (context) => const GuestPage(),
+        },
       ),
     );
   }
