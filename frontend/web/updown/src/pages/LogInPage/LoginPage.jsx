bcc60ad640bdb8a7db73ab41147a38fb96d5dbc3
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-19 15:28:39 +0200

    Frontend - Web => adding SignUp function into loginpage

--- frontend/web/updown/src/pages/LogInPage/LoginPage.jsx
+++ frontend/web/updown/src/pages/LogInPage/LoginPage.jsx
@@ -1,30 +1,58 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import TopBar from "../../components/TopBar/TopBar";
 import "./LoginPage.css";
+import request from "../../config/axios";
 
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const SignUp = () => {
+    const data = {
+      email,
+      password,
+    };
+    request({
+      method: "post",
+      url: "login",
+      data,
+    })
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem("token", response.token);
+        localStorage.setItem("user", JSON.stringify(response.result));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <>
       <TopBar />
-      <div className="login">
-        <Input
-          type={"email"}
-          label={"Email"}
-          placeholder={"Enter your Email"}
-        />
-        <Input
-          type={"password"}
-          label={"Password"}
-          placeholder={"Enter your Password"}
-        />
-        <Button
-          mode={"primary"}
-          onClick={null}
-          text={"Signup"}
-          width={"31rem"}
-        />
+      <div className="login-container">
+        <div className="login">
+          <Input
+            type={"email"}
+            label={"Email"}
+            placeholder={"Enter your Email"}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            type={"password"}
+            label={"Password"}
+            placeholder={"Enter your Password"}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            mode={"primary"}
+            onClick={SignUp}
+            text={"Signup"}
+            width={"big"}
+          />
+        </div>
       </div>
     </>
   );
