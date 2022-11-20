a1dba22e491ae7e7cb7b6b97ca751bbbf23a75e6
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-20 14:18:11 +0200

    Frontend - Web => adding home page

--- frontend/web/updown/src/App.js
+++ frontend/web/updown/src/App.js
@@ -3,6 +3,7 @@ import LoginPage from "./pages/LogInPage/LoginPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SideBar from "./components/SideBar/SideBar";
 import SideBarLayout from "./pages/SideBarLayout/SideBarLayout";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route element={<SideBarLayout />}>
-              <Route path="/home" element={<h1>Home</h1>} />
+              <Route path="/home" element={<HomePage />} />
               <Route path="/stats" element={<h1>Stats</h1>} />
               <Route path="/access" element={<h1>Access</h1>} />
             </Route>
