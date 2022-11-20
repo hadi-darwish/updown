306cd59e0621230c7c3cb2416753ac3d718d8f8b
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-20 16:49:48 +0200

    Frontend - Web => adding data  into home page

--- frontend/web/updown/src/pages/HomePage/HomePage.jsx
+++ frontend/web/updown/src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,93 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../../components/Button/Button";
+import request from "../../config/axios";
 import "./HomePage.css";
-import Dropdown from "react-dropdown";
-import "react-dropdown/style.css";
-import ReactDropdown from "react-dropdown";
-import { DropDownList } from "@progress/kendo-react-dropdowns";
 
 const HomePage = () => {
+  const [buildingName, setBuildingName] = useState("");
+  const [tax, setTax] = useState(0);
+  const [buildingId, setBuildingId] = useState(0);
+  const [price_per_travel, setPrice_per_travel] = useState(0);
+  const [isOn, setIsOn] = useState(0);
+  const [body, setBody] = useState("");
+
+  const send = () => {
+    const config = {
+      Host: "smtp.elasticemail.com",
+      Username: "vifodi7767@pamaweb.com",
+      Password: "72DFADEB3EB5DFDF9066B80C94E8A3B45119",
+      Port: "2525",
+      //   SecureToken: "c911847c-cc5b-455c-8184-ffe3ec2a4315",
+      To: "vifodi7767@pamaweb.com",
+      From: "vifodi7767@pamaweb.com",
+      Subject: "This is the subject",
+      Body: body,
+    };
+
+    if (window.Email) {
+      window.Email.send(config).then(function (message) {
+        alert("mail sent successfully");
+      });
+      setBody("");
+    }
+  };
+
+  useEffect(() => {
+    request({
+      method: "get",
+      url: "owner_building",
+    })
+      .then((response) => {
+        console.log(response);
+        setBuildingId(response.building.id);
+        setBuildingName(response.building.name);
+        setIsOn(response.building.is_on);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    request({
+      method: "post",
+      url: "building_price",
+      data: {
+        building_id: buildingId,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        setTax(response.prices[0].tax);
+        setPrice_per_travel(response.prices[0].price_per_travel);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [isOn]);
+
+  const toggle = () => {
+    request({
+      method: "put",
+      url: "elevator_status",
+      data: {
+        building_id: buildingId,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        setIsOn(!isOn);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="home-container">
       <h1>Home</h1>
       <div className="building-info">
-        <div>Building Name</div>
-        <div>Tax = $10</div>
-        <div>Price/Travel = $0.3</div>
+        <div>{buildingName}</div>
+        <div>Tax = ${tax}</div>
+        <div>Price/Travel = ${price_per_travel}</div>
         <div>
           <Button
             mode={"primary"}
@@ -26,7 +100,12 @@ const HomePage = () => {
       <div className="building-info">
         <div>Elevator Status</div>
         <div>
-          <Button mode={"primary"} onClick={null} text={"ON"} width={"small"} />
+          <Button
+            mode={"primary"}
+            onClick={toggle}
+            text={isOn === 1 ? "ON" : "OFF"}
+            width={"small"}
+          />
         </div>
       </div>
       <h1>Notify Residents</h1>
@@ -38,9 +117,11 @@ const HomePage = () => {
         placeholder="Enter message here"
         rows="4"
         cols="50"
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
       />
       <div className="button">
-        <Button mode={"primary"} onClick={null} text={"Send"} width={"big"} />
+        <Button mode={"primary"} onClick={send} text={"Send"} width={"big"} />
       </div>
     </div>
   );
