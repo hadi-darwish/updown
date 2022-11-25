e5a1d8aeaa724a8cb7c7aa5ca77cb2d2fdb9836e
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-25 19:16:33 +0200

    Backend => adding updatePrice function into price controller

--- backend/updown-server/app/Http/Controllers/PriceController.php
+++ backend/updown-server/app/Http/Controllers/PriceController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Building;
+use App\Models\Price;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class PriceController extends Controller
 {
@@ -19,6 +22,37 @@ class PriceController extends Controller
                 'status' => 'error',
                 'message' => 'Price not created',
             ], 404);
+        }
+    }
+
+    public function updatePrice(Request $request)
+    {
+        $price = Building::find($request->building_id)->prices->last();
+        if (!$price) {
+            $prices = new Price();
+            $prices->building_id = $request->building_id;
+            $prices->tax = 0;
+            $prices->price_per_travel = 0;
+            $prices->start_date = Carbon::now();
+            $prices->end_date = Carbon::now()->addMonth();
+            if ($prices->save()) {
+                $prices->update($request->all());
+                return response()->json([
+                    'status' => 'success',
+                    'price' => $price,
+                ]);
+            } else {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Price not created',
+                ], 404);
+            }
+        } else {
+            $price->update($request->all());
+            return response()->json([
+                'status' => 'success',
+                'price' => $price,
+            ]);
         }
     }
 }
