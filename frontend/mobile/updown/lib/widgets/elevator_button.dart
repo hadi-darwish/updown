99a984c7e1b28cf93231607114a30412b92fdcc6
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-26 16:46:10 +0200

    Frontend - Mobile => fixing elevator button

--- frontend/mobile/updown/lib/widgets/elevator_button.dart
+++ frontend/mobile/updown/lib/widgets/elevator_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:updown/providers/building_provider.dart';
 import 'package:updown/providers/travel_provider.dart';
 
 class ElevatorButton extends StatefulWidget {
@@ -19,6 +21,9 @@ class _ElevatorButtonState extends State<ElevatorButton> {
   bool isPressed = false;
   @override
   Widget build(BuildContext context) {
+    var isOn = Provider.of<Travel>(context);
+    var status = Provider.of<Building>(context);
+    print('$isOn');
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
@@ -28,18 +33,32 @@ class _ElevatorButtonState extends State<ElevatorButton> {
           setState(() {
             isPressed = !isPressed;
           });
+          if (status.isOn == 0) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Elevator is OFF'),
+              ),
+            );
+            setState(() {
+              isPressed = !isPressed;
+            });
+            return;
+          }
+
           await Travel().requestTravel(widget.floorNumber, widget.currentFloor);
           Future delayed = Future.delayed(const Duration(seconds: 1), () {
             showDialog(
               context: context,
-              builder: (BuildContext context) {
+              builder: (BuildContext ctx) {
                 return AlertDialog(
                   title: const Text('Travel Request'),
                   content: const Text('Your travel request has been sent'),
                   actions: [
                     TextButton(
                       onPressed: () {
-                        Navigator.of(context).pop();
+                        Navigator.of(ctx).pop();
+                        isOn.AnimationFrom(
+                            widget.floorNumber, widget.currentFloor, context);
                       },
                       child: const Text('OK'),
                     ),
@@ -62,7 +81,11 @@ class _ElevatorButtonState extends State<ElevatorButton> {
         backgroundColor: widget.disabled
             ? MaterialStateProperty.all<Color>(
                 Theme.of(context).primaryColor.withOpacity(0.5))
-            : MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
+            : isOn.onFloor == widget.floorNumber
+                ? MaterialStateProperty.all<Color>(
+                    Theme.of(context).secondaryHeaderColor)
+                : MaterialStateProperty.all<Color>(
+                    Theme.of(context).primaryColor),
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(5.0),
