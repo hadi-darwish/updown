71d67b498ef68555a5d10c3ec72df257e1907a58
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-26 16:30:06 +0200

    Backend => adding users route

--- backend/updown-server/routes/api.php
+++ backend/updown-server/routes/api.php
@@ -20,4 +20,5 @@ Route::post('travel', [UserController::class, 'createTravel']);
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('user', [AuthController::class, 'user']);
     Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('users', [UserController::class, 'getAllUsers']);
 });
