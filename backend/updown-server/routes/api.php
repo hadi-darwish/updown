f432900aceb5104833f3b4cbaaf02007ea101f75
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-26 16:27:55 +0200

    Backend => adding rm route

--- backend/updown-server/routes/api.php
+++ backend/updown-server/routes/api.php
@@ -13,3 +13,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('guest', [UserController::class, 'enterGuest']);
+Route::post('rm', [UserController::class, 'getUserBuilding']);
