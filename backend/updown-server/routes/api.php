cdf1a12ff7fcbb5fa223190f844d3f76b5d1535b
hadi-darwish <hadi.darwish.03@gmail.com>
2022-11-26 16:27:30 +0200

    Backend => adding guest route

--- backend/updown-server/routes/api.php
+++ backend/updown-server/routes/api.php
@@ -2,9 +2,14 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ApartmentController;
+use App\Http\Controllers\PriceController;
+use App\Http\Controllers\BuildingController;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::post('guest', [UserController::class, 'enterGuest']);
